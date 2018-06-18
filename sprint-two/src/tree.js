1f948a469d57d564fcfbfef2aebf95598faa51e8
Meifang Lin <meiflin123@gmail.com>
2018-06-18 00:39:58 -0700

    completed tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,28 @@
+// every child inherit all properties.
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];// fix me
 
   return newTree;
 };
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+  return false;
 };
-
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
