86c62210d3fd9fa3d677d9d0e377ed2e00ec9147
Meifang Lin <meiflin123@gmail.com>
2018-06-17 19:52:23 -0700

    completed linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var val = Node(value);
+    list.tail = val;
+    if (list.head === null) {
+      list.head = val;
+    } else {
+      var headNext = list.head.next;
+      headNext = val;
+      list.head.next = headNext;
+    }
   };
 
   list.removeHead = function() {
+    var formerHead = list.head.value;
+    list.head = list.head.next;
+    return formerHead;
+
   };
 
   list.contains = function(target) {
+    var node = this.head;
+    var result = true;
+    while (node) {
+      if (node.value === target) {
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
   };
-
+  
   return list;
 };
 
